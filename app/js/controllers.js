d2dcb54f36a424c3518e365bc47aaa7793907a04
Arup Sen <arupsen48@gmail.com>
2014-09-08 17:53:29 +0100

    set up basic thumbnail functionality for the products page and added images

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,26 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('ProductsController', [function() {
+  controller('ProductsController', ['$scope', function($scope) {
+    // products holds data in here for now
+    $scope.products = [
+  {imgPath: 'MS-2014-1-01'},
+  {imgPath: 'MS-2014-1-02'},
+  {imgPath: 'MS-2014-1-03'},
+  {imgPath: 'MS-2014-1-04'},
+  {imgPath: 'MS-2014-1-05'},
+  {imgPath: 'MS-2014-1-06'},
+  {imgPath: 'MS-2014-1-07'},
+  {imgPath: 'MS-2014-1-08'},
+  {imgPath: 'MS-2014-1-09'},
+  {imgPath: 'MS-2014-1-10'},
+  {imgPath: 'MS-2014-1-11'},
+  {imgPath: 'MS-2014-1-12'},
+  {imgPath: 'MS-2014-1-13'},
+  {imgPath: 'MS-2014-1-14'},
+  {imgPath: 'MS-2014-1-15'},
+  {imgPath: 'MS-2014-1-16'}
+    ];
 
   }])
   .controller('AboutController', [function() {
