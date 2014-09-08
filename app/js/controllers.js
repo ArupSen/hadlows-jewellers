4ec569f99fc58d8fb760c40b45867922f02c1166
Arup Sen <arupsen48@gmail.com>
2014-09-08 17:00:21 +0100

    created an about and products page set up with some text on about

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('ProductsController', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('AboutController', [function() {
 
   }]);
